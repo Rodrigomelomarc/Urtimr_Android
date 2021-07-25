64df40db832b69be69b35796fb3df689d5501b71
Rodrigo Melo <rodrigomelomarcolino@gmail.com>
2021-07-25 01:13:21 -0300

    Criação das telas de auth

---
+++ app/src/main/java/com/hiperion/urtimr/UI/Activities/PasswordRecoveryActivity.kt
@@ -0,0 +1,12 @@
+package com.hiperion.urtimr.UI.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hiperion.urtimr.R
+
+class PasswordRecoveryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_password_recovery)
+    }
+}
---
+++ app/src/main/java/com/hiperion/urtimr/UI/Activities/SignInActivity.kt
@@ -0,0 +1,13 @@
+package com.hiperion.urtimr.UI.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hiperion.urtimr.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+    }
+}
